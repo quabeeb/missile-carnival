72da1844cf40d1180598e50c6f808a9d87fd8880
Andrew Lee <quabeeb@gmail.com>
2020-05-25 16:54:03 -0400

    Only shoot straight lines for now

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ extern crate rand;
 use ggez::{input, graphics, Context, ContextBuilder, GameResult};
 use ggez::event::{self, EventHandler, KeyCode};
 use ggez::timer;
-use rand::Rng;
 
 mod missile;
 mod position;
 
-const DESIRED_FPS: u32 = 60;
+const DESIRED_FPS: u32 = 240;
+
 
 fn main() {    
     let (mut ctx, mut event_loop) = ContextBuilder::new("cuban-missie-crisis-test", "Andy")
@@ -71,10 +71,20 @@ impl EventHandler for State {
         }
 
         if pressed_keys.contains(&KeyCode::Z) {
-            let new_right_missile = missile::Missile::new(self.missile_spawning_position, 1);
-            let new_left_missile = missile::Missile::new(self.missile_spawning_position, -1);
+            let right_position = position::Position::new(self.missile_spawning_position.x + 15, self.missile_spawning_position.y);
+            let left_position = position::Position::new(self.missile_spawning_position.x - 15, self.missile_spawning_position.y);
+            let right_right_position = position::Position::new(self.missile_spawning_position.x + 30, self.missile_spawning_position.y);
+            let left_left_position = position::Position::new(self.missile_spawning_position.x - 30, self.missile_spawning_position.y);
+
+            let new_right_right_missile = missile::Missile::new(right_right_position);
+            let new_left_left_missile = missile::Missile::new(left_left_position);
+            let new_right_missile = missile::Missile::new(right_position);
+            let new_left_missile = missile::Missile::new(left_position);
+
             self.missiles.push(new_right_missile);
             self.missiles.push(new_left_missile);
+            self.missiles.push(new_right_right_missile);
+            self.missiles.push(new_left_left_missile);
         }
 
         while timer::check_update_time(ctx, DESIRED_FPS) {            
@@ -82,17 +92,19 @@ impl EventHandler for State {
                 m.get_new_position();
             }
 
-            self.missiles.retain(|x| x.current_position.y > 0);
+            self.missiles.retain(|x| x.current_position.y > -1);
         }
 
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        // let missile_image = graphics::Image::new(ctx, "../static/graze-box-rg.png")?;
+        // let missile_batch = graphics::spritebatch::SpriteBatch::new(missile_image);
+    
 
         graphics::clear(ctx, graphics::WHITE);
 
-        let mut rng = rand::thread_rng();
         let player_color = [0.0, 0.0, 1.0, 1.0].into();
         let missile_color = [1.0, 0.0, 0.0, 1.0].into();
 
@@ -110,16 +122,15 @@ impl EventHandler for State {
 
         graphics::draw(ctx, &missile_spawner, (ggez::mint::Point2 { x: 0.0, y: 0.0 },))?;
 
-
         for m in &self.missiles {
             let rectangle = graphics::Mesh::new_rectangle(
                 ctx,
                 graphics::DrawMode::fill(),
                 graphics::Rect::new_i32(
                     m.current_position.x,
-                    m.current_position.y,
+                    -600 + m.current_position.y,
                     10,
-                    10
+                    600
                 ),
                 missile_color
             )?;
--- src/missile.rs
+++ src/missile.rs
@@ -4,33 +4,18 @@ use ggez::{Context, GameResult};
 
 #[derive(PartialEq, Eq, Hash)]
 pub struct Missile {
-    initial_position: position::Position,
-    pub current_position: position::Position,
-    timesteps: i32,
-    incrementer: i32
+    pub current_position: position::Position
 }
 
 impl Missile {
-    pub fn new(pos: position::Position, increment: i32) -> Self {
+    pub fn new(pos: position::Position) -> Self {
         Missile {
-            initial_position: pos,
             current_position: pos,
-            timesteps: 0,
-            incrementer: increment
         }
     }
 
     pub fn get_new_position(&mut self) -> position::Position {
-        let ip = self.initial_position;
-
-        self.timesteps += self.incrementer;
-
-        let new_x = ip.x + self.timesteps;
-        let new_y = ip.y - self.timesteps.pow(2)/5;
-        
-        let new_position = position::Position::new(new_x, new_y);
-        self.current_position = new_position;
-
-        new_position
+        self.current_position.y -= 10;
+        self.current_position
     }
 }
